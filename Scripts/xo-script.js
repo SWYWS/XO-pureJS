ead99672bfad187801f5a8f3db474cbde13504bb
Alexander <swyws@yandex.ru>
2020-05-12 19:48:22 +0300

    difficulty choosing was added

--- Scripts/xo-script.js
+++ Scripts/xo-script.js
@@ -1,156 +1,187 @@
 "use strict"
 
-let XOField;
-let XOForm = document.querySelector(".xo-form");
-let XOSettings = {
+let xoForm = document.querySelector(".xo-form");
+let xoSettings = {
   height: 3,
   width: 3,
-  playerSide: "X",
-  botSide: "O"
+  huPlayer: "X",
+  aiPlayer: "O",
+  difficulty: "easy"
 };
 
 
 
 startGame.addEventListener("click", function () {
-  for (let side of XOForm.sides) {
-    if (side.checked) {
-      XOSettings.playerSide = side.id;
-    } else XOSettings.botSide = side.id;
+  for (let player of xoForm.players) {
+    if (player.checked) {
+      xoSettings.huPlayer = player.id;
+    } else xoSettings.aiPlayer = player.id;
   }
-  XOForm.style.visibility = "hidden";
-  newXOGameInit(XOSettings);
+  for (let diff of xoForm.difficulty) {
+    if (diff.checked) {
+      xoSettings.difficulty = diff.id;
+      break;
+    }
+  }
+  xoForm.style.visibility = "hidden";
+  xoGameInit(xoSettings);
 });
 
 
 
-function newXOGameInit(settings) {
-  if (XOField) XOField.remove();
+function xoGameInit(settings) {
+  let currentField = document.querySelector(".xo-field");
+  if (currentField) currentField.remove();
 
-  createXOField(settings.height, settings.width);
-  let cells = Array.from(document.querySelectorAll('.xo-cell'));
-  XOField = document.querySelector(".xo-field");
+  let xoArr = createXOArr();
+  let xoField = createXOField();
+  xoField.addEventListener("click", xoClick);
+  xoField.addEventListener("contextmenu", event => event.preventDefault());
+  let getIdAi = (function () {
+    switch (settings.difficulty) {
+      default:
+      case "easy":
+        return getRandomId;
 
-  if (settings.botSide == "X") placeXO(cells[randomInt(cells.length)], settings.botSide);
+      case "hard":
+        return minimax;
+    }
+  })();
 
-  XOField.addEventListener("click", xoClick);
-  XOField.addEventListener("dblclick", (event) => event.preventDefault());
-  XOField.addEventListener("contextmenu", (event) => event.preventDefault());
-
-
-  function createXOField(lenX, lenY) {
+  function createXOField() {
     let field = document.createElement("div");
     field.className = "xo-field";
     document.querySelector(".xo-game").append(field);
-    for (let i = 0; i < lenY; i++) {
-      for (let j = 0; j < lenX; j++) {
-        let newElem = document.createElement("div");
-        newElem.className = "xo-cell";
-        newElem.id = i + "_" + j;
-        field.append(newElem);
+    for (let i = 0; i < settings.width * settings.height; i++) {
+      let newElem = document.createElement("div");
+      newElem.className = "xo-cell";
+      newElem.id = i;
+      field.append(newElem);
+      if (i % settings.width == settings.height - 1) {
+        field.append(document.createElement("br"));
       }
-      field.append(document.createElement("br"));
     }
+    return field;
   }
 
+  function createXOArr() {
+    let arr = new Array;
+    for (let i = 0; i < settings.width * settings.height; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }
 
   function xoClick(event) {
     let target = event.target;
+    if (target.className != "xo-cell" || target.textContent != "") return;
 
-    if (target.className != "xo-cell" || target.textContent) return;
+    target.textContent = settings.huPlayer;
+    xoArr[target.id] = settings.huPlayer;
 
-    placeXO(target, settings.playerSide);
+    if (xoWin_3x3(xoArr, settings.huPlayer)) {
+      showFinalAlert("won");
+      this.removeEventListener("click", xoClick);
+      return;
+    } else if (getEmptyIds(xoArr).length == 0) {
+      showFinalAlert("Draw");
+      this.removeEventListener("click", xoClick);
+      return;
+    }
 
-    if (xoGameOver(isOver(settings))) return;
+    let id = getIdAi(xoArr, settings.aiPlayer).index;
+    document.getElementById(id).textContent = settings.aiPlayer;
+    xoArr[id] = settings.aiPlayer;
 
-    placeXO(cells[randomInt(cells.length)], settings.botSide);
-
-    xoGameOver(isOver(settings));
-  }
-
-
-  function placeXO(elem, str) {
-    if (cells.length == 0) return;
-
-    elem.textContent = str;
-    cells.splice(cells.findIndex((item) => item === elem), 1);
-  }
-
-
-  function xoGameOver(result) {
-    switch (result) {
-      default:
-      case false:
-        break;
-
-      case settings.playerSide:
-        showFinalAlert("won");
-        XOField.removeEventListener("click", xoClick);
-        return true;
-
-      case settings.botSide:
-        showFinalAlert("lost");
-        XOField.removeEventListener("click", xoClick);
-        return true;
-
-      case "Draw":
-        showFinalAlert("Draw");
-        XOField.removeEventListener("click", xoClick);
-        return true;
+    if (xoWin_3x3(xoArr, settings.aiPlayer)) {
+      showFinalAlert("lost");
+      this.removeEventListener("click", xoClick);
+    } else if (getEmptyIds(xoArr).length == 0) {
+      showFinalAlert("Draw");
+      this.removeEventListener("click", xoClick);
+      return;
     }
   }
 
-
-  function isOver(settings) {
-    let val_00 = document.getElementById("0_0").textContent,
-      val_11 = document.getElementById("1_1").textContent,
-      val_22 = document.getElementById("2_2").textContent;
-  
-    if (val_00 && val_00 == val_11 && val_00 == val_22) {
-      return val_00 == settings.playerSide ? settings.playerSide : settings.botSide;
-    }
-  
-    let val_02 = document.getElementById("0_2").textContent,
-      val_20 = document.getElementById("2_0").textContent;
-  
-    if (val_02 && val_02 == val_11 && val_02 == val_20) {
-      return val_02 == settings.playerSide ? settings.playerSide : settings.botSide;
-    }
-  
-    for (let i = 0; i < 3; i++) {
-      let val_i0 = document.getElementById(i + "_0").textContent,
-        val_i1 = document.getElementById(i + "_1").textContent,
-        val_i2 = document.getElementById(i + "_2").textContent;
-  
-      if (val_i0 && val_i0 == val_i1 && val_i0 == val_i2) {
-        return val_i0 == settings.playerSide ? settings.playerSide : settings.botSide;
-      }
-  
-      let val_0i = document.getElementById("0_" + i).textContent,
-        val_1i = document.getElementById("1_" + i).textContent,
-        val_2i = document.getElementById("2_" + i).textContent;
-  
-      if (val_0i && val_0i == val_1i && val_0i == val_2i) {
-        return val_0i == settings.playerSide ? settings.playerSide : settings.botSide;
-      }
-    }
-
-    if (cells.length == 0) return "Draw";
+  function xoWin_3x3(arr, player) {
+    if ((arr[0] == player && arr[1] == player && arr[2] == player)
+      || (arr[3] == player && arr[4] == player && arr[5] == player)
+      || (arr[6] == player && arr[7] == player && arr[8] == player)
+      || (arr[0] == player && arr[3] == player && arr[6] == player)
+      || (arr[1] == player && arr[4] == player && arr[7] == player)
+      || (arr[2] == player && arr[5] == player && arr[8] == player)
+      || (arr[0] == player && arr[4] == player && arr[8] == player)
+      || (arr[2] == player && arr[4] == player && arr[6] == player))
+      return true;
 
     return false;
   }
 
+  function getEmptyIds(arr) {
+    return arr.filter(item => item != settings.huPlayer && item != settings.aiPlayer);
+  }
+
+  function minimax(arr, player) {
+    let availIds = getEmptyIds(arr);
+
+    if (xoWin_3x3(arr, settings.huPlayer)) {
+      return { score: -10 };
+    } else if (xoWin_3x3(arr, settings.aiPlayer)) {
+      return { score: 10 };
+    } else if (availIds.length === 0) {
+      return { score: 0 };
+    }
+
+    let moves = new Array;
+    for (let i = 0; i < availIds.length; i++) {
+      let move = {};
+      move.index = arr[availIds[i]];
+      arr[availIds[i]] = player;
+
+      if (player == settings.aiPlayer) {
+        let result = minimax(arr, settings.huPlayer);
+        move.score = result.score;
+      } else {
+        let result = minimax(arr, settings.aiPlayer);
+        move.score = result.score;
+      }
+
+      arr[availIds[i]] = move.index;
+      moves.push(move);
+    }
+
+    let bestMove;
+    if (player === settings.aiPlayer) {
+      let bestScore = -99999;
+      for (let i = 0; i < moves.length; i++) {
+        if (moves[i].score > bestScore) {
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    } else {
+      let bestScore = 99999;
+      for (let i = 0; i < moves.length; i++) {
+        if (moves[i].score < bestScore) {
+          bestScore = moves[i].score;
+          bestMove = i;
+        }
+      }
+    }
+    return moves[bestMove];
+  }
+
+  function getRandomId(arr) {
+    let availIds = getEmptyIds(arr);
+    return { index: availIds[Math.floor(Math.random() * (availIds.length))] };
+  }
 
   function showFinalAlert(message) {
     let alert = document.createElement("div");
     alert.className = "xo-result " + message;
     alert.innerHTML = (message == "Draw" ? "" : "You ") + message + "!<br>" + "Wanna play again? "
-      + "<a href='#' class='new-game-button' id='XONewGame'>yes</a>"
-    XOField.append(alert);
-    XONewGame.addEventListener("click", () => XOForm.style.visibility = "visible");
-  }
-  
-
-  function randomInt(max) {
-    return Math.floor(Math.random() * max);
+      + "<a href='#' class='new-game-button' id='xoNewGame'>yes</a>"
+    xoField.append(alert);
+    xoNewGame.addEventListener("click", () => xoForm.style.visibility = "visible");
   }
 }
